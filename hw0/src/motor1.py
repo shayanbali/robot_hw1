5a301859a2e21c1f14ae29beced4233b338739a0
shayanbali <shayanbali@gmail.com>
2023-04-02 15:14:09 -0700

    Add msgs and motors

---
+++ hw0/src/motor1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import rospy
+from hw0.msg import direction
+
+def callback(data):
+    rt = "clockwise"
+    if data.rotate == -1:
+        rt = "counter clockwise"
+    elif data.rotate == 0:
+        rt = "fixed"
+    rospy.loginfo(rospy.get_caller_id() + '\n%s ' + rt, data)
+
+def listener():
+
+    rospy.init_node('motor11', anonymous=True)
+
+    rospy.Subscriber('motor1', direction, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
